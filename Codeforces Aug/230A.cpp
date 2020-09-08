d344cc9e391c9d79c9e0499e1c76f048926dc053
Kavita Verma <kavita12180@gmail.com>
2020-09-09 00:16:25 +0530

    first commit

---
+++ Codeforces Aug/230A.cpp
@@ -0,0 +1,17 @@
+//230 A Dragons
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int s, n, i, j, a, b;
+    cin>>s>>n;
+    multiset<int> x;
+    for( i=0; i<n; i++ )
+    {
+        cin>>a>>b;
+    }
+    return 0;
+}
