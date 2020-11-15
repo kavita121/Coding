605fd65907709d9b88333fd1274fa1a74dd39f27
Kavita Verma <kavita12180@gmail.com>
2020-11-15 23:59:53 +0530

    15 nov 20

---
+++ 121.BestTimetoBuyandSellStock.cpp
@@ -0,0 +1,17 @@
+//121. Best Time to Buy and Sell Stock
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int minPrice = 999999, max=0;
+        for(int i=0; i<prices.size(); i++)
+        {
+            if(minPrice > prices[i])
+                minPrice = prices[i];
+            if(max < prices[i]-minPrice)
+                max = prices[i]-minPrice;
+        }
+        return max;
+    }
+};
