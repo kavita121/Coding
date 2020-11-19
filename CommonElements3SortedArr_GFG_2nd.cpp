0eddc4726dd776544213d1e0dedaa026bd056f35
Kavita Verma <kavita12180@gmail.com>
2020-11-20 00:16:35 +0530

    19 nov 20

--- CommonElements3SortedArr_GFG_2nd.cpp
+++ CommonElements3SortedArr_GFG_2nd.cpp
@@ -35,6 +35,38 @@ class Solution
 
 };
 
+//To deal with duplicacy we can use a set
+/*
+
+vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
+        {
+            set<int> c;
+            vector<int> common;
+            int i, j, k;
+            i = j = k = 0;
+            while( i<n1 && j<n2 && k<n3)
+            {
+                if(A[i] == B[j] && B[j] == C[k])
+                {
+                    c.insert(A[i]);
+                    i++;
+                    j++;
+                    k++;
+                }
+                else if(A[i] < B[j])
+                    i++;
+                else if(B[j] < C[k])
+                    j++;
+                else
+                    k++;
+            }
+            for(auto itr = c.begin(); itr != c.end(); itr++)
+                common.push_back(*itr);
+            return common;
+            
+        }
+
+*/
 
 
 int main ()
