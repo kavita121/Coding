df6e9e05ed8bc81b275827b99bc162df9d159d05
kavita121 <kavita12180@gmail.com>
2020-12-23 20:34:18 +0530

    23 dec 20

---
+++ CountSquares_GFG.cpp
@@ -0,0 +1,36 @@
+//Count Squares
+//GFG ACCEPTED
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+  public:
+    int countSquares(int N) 
+    {
+        int count = 0;
+        for(int i=1; i<=sqrt(N); i++)
+        {
+            if(i*i < N)
+                count++;
+        }
+        return count;
+    }
+};
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.countSquares(N) << endl;
+    }
+    return 0;
+}
